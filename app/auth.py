5cb965144fc19322eeb01069087f3284cb3158eb
shy0401 <166055700+shy0401@users.noreply.github.com>
2024-12-20 02:40:43 +0900

    jwt token14

--- app/auth.py
+++ app/auth.py
@@ -9,7 +9,7 @@ users = {"admin": "password"}
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
-    username = data.get('13')
+    username = data.get('14')
     password = data.get('password130')
     if username in users and users[username] == password:
         token = create_access_token(identity=username)
